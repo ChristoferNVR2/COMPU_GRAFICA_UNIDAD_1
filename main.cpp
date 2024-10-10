2dcbcc898dbe748103578258a5f577fe7a4ad053
chrisvega <cnvegaro@unitru.edu.pe>
2024-10-09 22:01:51 -0500

    Initial cube

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
-#include <GL/glew.h> // Must be included before GLFW
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <sstream>
 #include <csignal>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-// Platform-specific debug break
 #ifdef __linux__
-    #define debugBreak() raise(SIGTRAP)  // Linux equivalent for debug break
+    #define debugBreak() raise(SIGTRAP)
 #elif _WIN32
-    #define debugBreak() __debugbreak()  // Windows debug break
+    #define debugBreak() __debugbreak()
 #endif
 
 #define ASSERT(x) if (!(x)) debugBreak();
@@ -38,7 +39,6 @@ struct ShaderProgramSource {
 
 static ShaderProgramSource ParseShader(const std::string& filepath) {
     std::ifstream stream(filepath);
-
     enum class ShaderType {
         NONE = -1, VERTEX = 0, FRAGMENT = 1
     };
@@ -53,18 +53,15 @@ static ShaderProgramSource ParseShader(const std::string& filepath) {
             else if (line.find("fragment") != std::string::npos)
                 type = ShaderType::FRAGMENT;
         } else {
-            // ss[(int) type] << line << '\n';  // C-style cast
-            ss[static_cast<int>(type)] << line << '\n';  // C++ cast
+            ss[static_cast<int>(type)] << line << '\n';
         }
     }
-
     return { ss[0].str(), ss[1].str() };
 }
 
-
 static unsigned int CompileShader(unsigned int type, const std::string& source) {
     unsigned int id = glCreateShader(type);
-    const char* src = source.c_str();  // Get the pointer to the first element of the string
+    const char* src = source.c_str();
     glShaderSource(id, 1, &src, nullptr);
     glCompileShader(id);
 
@@ -73,18 +70,15 @@ static unsigned int CompileShader(unsigned int type, const std::string& source) 
     if (result == GL_FALSE) {
         int length;
         glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        // char* message = (char*)alloca(length * sizeof(char));  // C-style cast
-        char* message = static_cast<char*>(alloca(length * sizeof(char)));  // C++ cast
+        char* message = static_cast<char*>(alloca(length * sizeof(char)));
         glGetShaderInfoLog(id, length, &length, message);
         std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
         std::cout << message << std::endl;
         glDeleteShader(id);
         return 0;
     }
-
     return id;
 }
-
 
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
     GLCall(unsigned int program = glCreateProgram());
@@ -102,115 +96,97 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
 }
 
 int main() {
-    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-
-    glewInit();
-
-    if (!window)
-    {
+    GLFWwindow* window = glfwCreateWindow(640, 480, "3D Cube", nullptr, nullptr);
+    if (!window) {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
-
     glfwSwapInterval(1);
 
-    if (glewInit() != GLEW_OK)
-        std::cout << "Error!" << std::endl;
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Error initializing GLEW!" << std::endl;
+        return -1;
+    }
 
-    std::cout << glGetString(GL_VERSION) << std::endl;
-
+    // Enable depth test for correct 3D rendering
+    glEnable(GL_DEPTH_TEST);
 
     float positions[] = {
-        -0.5f, -0.5f, 0.5,// 0
-         0.5f, -0.5f, -0.5,// 1
-         0.5f,  0.5f, 0.5// 2
-        -0.5f,  0.5f,  0.5// 3
+        // Front face
+        -0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        // Back face
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f
     };
 
     unsigned int indices[] = {
-        0, 1, 2,
-        2, 3, 0
+        // Front
+        0, 1, 2, 2, 3, 0,
+        // Back
+        4, 5, 6, 6, 7, 4,
+        // Left
+        0, 3, 7, 7, 4, 0,
+        // Right
+        1, 2, 6, 6, 5, 1,
+        // Top
+        3, 2, 6, 6, 7, 3,
+        // Bottom
+        0, 1, 5, 5, 4, 0
     };
 
-    unsigned int vao;
+    unsigned int vao, vbo, ibo;
     GLCall(glGenVertexArrays(1, &vao));
     GLCall(glBindVertexArray(vao));
 
-    unsigned int buffer;
-    GLCall(glGenBuffers(1, &buffer));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-    GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
+    GLCall(glGenBuffers(1, &vbo));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, vbo));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW));
 
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, nullptr);
+    GLCall(glEnableVertexAttribArray(0));
+    GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr));
 
-    // Index Buffer Object
-    unsigned int ibo;
     GLCall(glGenBuffers(1, &ibo));
     GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW));
 
-
-    // Don't forget to set the working directory to the project root directory
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
 
-    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
-    ASSERT(location != -1);
-    GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+    glm::mat4 proj = glm::perspective(glm::radians(45.0f), 640.0f / 480.0f, 0.1f, 100.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, -3));
+    glm::mat4 model = glm::rotate(glm::mat4(1.0f), static_cast<float>(glfwGetTime()), glm::vec3(0.5f, 1.0f, 0.0f));
+    glm::mat4 mvp = proj * view * model;
 
-    GLCall(glBindVertexArray(0));
-    GLCall(glUseProgram(0));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+    int mvpLocation = glGetUniformLocation(shader, "u_MVP");
+    GLCall(glUniformMatrix4fv(mvpLocation, 1, GL_FALSE, glm::value_ptr(mvp)));
 
-
-    float r = 0.0f;
-    float increment = 0.05f;
-
-    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        GLCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 
-        GLCall(glUseProgram(shader));
-        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+        model = glm::rotate(glm::mat4(1.0f), static_cast<float>(glfwGetTime()), glm::vec3(0.5f, 1.0f, 0.0f));
+        mvp = proj * view * model;
+        GLCall(glUniformMatrix4fv(mvpLocation, 1, GL_FALSE, glm::value_ptr(mvp)));
 
-        GLCall(glBindVertexArray(vao));
-        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+        GLCall(glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr));
 
-        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr))
-
-        if (r > 1.0f)
-            increment = -0.05f;
-        else if (r < 0.0f)
-            increment = 0.05f;
-
-        r += increment;
-
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
     glDeleteProgram(shader);
-
     glfwTerminate();
     return 0;
 }
+
+
